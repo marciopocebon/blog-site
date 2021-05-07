c7b8f9dbe92dbe69b0a895cd4c830c30baceb2a4
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-05-07 21:38:21 +0530

    fixing

--- pages/contact.js
+++ pages/contact.js
@@ -28,7 +28,7 @@ export default function ContactUs() {
   }
 
   return (
-    <div className="bg-[#023047] justify-between flex flex-col min-h-screen min-w-[200vw] md:flex-row md:pt-20">
+    <div className="bg-[#023047] justify-between flex flex-col min-h-screen md:flex-row pt-20">
       <Head>
         <title>Contact us</title>
         <link
@@ -36,14 +36,12 @@ export default function ContactUs() {
           href="https://res.cloudinary.com/dssvrf9oz/image/upload/v1620273120/image-removebg-preview_6_obe3fv.png"
         />
       </Head>
-      <div className="pl-1 md:pt-20 md:pl-20 w-[450px] pr-5 md:flex flex-col md:ml-0 ml-10">
-        <h2 className="mt-10 text-4xl font-bold text-gray-100 md:mt-0">
-          Help us improve
-        </h2>
+      <div className="md:pt-20 pl-20 w-[400px] pr-5">
+        <h2 className="text-4xl font-bold text-gray-100">Help us improve</h2>
         <p className="text-gray-200 pt-5">
           Fill up the form and I will get back to you
         </p>
-        <div className="hidden ring-blue-700 hover:ring-2 rounded-lg cursor-pointer focus:outline-none p-3 pr-1 transform-gpu mt-20 md:flex">
+        <div className="hidden md:flex ring-blue-400 hover:ring-2 rounded-lg cursor-pointer focus:outline-none p-3  transform-gpu pr-4 mt-20">
           <img
             src="https://res.cloudinary.com/dssvrf9oz/image/upload/v1620314548/931-9311379_email-icons-white-color-white-email-icon-png-removebg-preview_vzxdln.png"
             alt=""
@@ -52,7 +50,7 @@ export default function ContactUs() {
           <p className="text-gray-200 text-l">avneeshagarwal0612@gmail.com</p>
         </div>
 
-        <div className="hidden ring-blue-700 hover:ring-2 rounded-lg cursor-pointer focus:outline-none p-3 pr-1 transform-gpu mt-4 md:flex">
+        <div className="hidden md:flex ring-blue-400 hover:ring-2 rounded-lg cursor-pointer focus:outline-none p-3  transform-gpu pr-4 mt-5">
           <img
             src="https://res.cloudinary.com/dssvrf9oz/image/upload/v1620391308/154-1541366_transparent-edit-icon-png-location-white-icon-png-removebg-preview_1_ewvzdm.png"
             alt=""
@@ -60,7 +58,7 @@ export default function ContactUs() {
           />
           <p className="text-gray-200 text-l">India</p>
         </div>
-        <div className="hidden md:flex mt-20">
+        <div className="flex md:mt-20">
           <a href="https:instagram.com/avneesh__agarwal" target="_blank">
             <img
               className="h-14 w-14 hover:bg-blue-900 rounded-full cursor-pointer transform-gpu "
@@ -82,7 +80,7 @@ export default function ContactUs() {
       </div>
       <form
         onSubmit={sendEmail}
-        className="flex flex-col mb-8 mt-8 bg-white h-[650px] rounded-3xl ml-10 mr-8 md:w-2/5 md:mr-5 md:mb-0 mt:mt-0"
+        className="flex flex-col mb-8 mt-8 bg-white h-[650px] rounded-3xl ml-10 mr-8 md:w-2/5 md:mr-5 md:mb-0"
       >
         <div className="flex flex-col m-5">
           <h3 className="ml-2 text-blue-700 font-semibold text-md">
@@ -122,48 +120,12 @@ export default function ContactUs() {
           ></textarea>
           <button
             type="submit"
-            className="text-white bg-blue-500 md:m-2 w-52 md:w-60 h-12 rounded-lg  mt-8 sm:ml-auto"
+            className="text-white bg-blue-500 m-2 w-60 h-12 rounded-lg  mt-8 ml-auto"
           >
             Send Message
           </button>
         </div>
       </form>
-      <div className="flex ring-blue-700 hover:ring-2 p-2 mb-5 rounded-lg cursor-pointer focus:outline-none w-10/12 ml-10 transform-gpu md:hidden">
-        <img
-          src="https://res.cloudinary.com/dssvrf9oz/image/upload/v1620314548/931-9311379_email-icons-white-color-white-email-icon-png-removebg-preview_vzxdln.png"
-          alt=""
-          className="h-6 ml-2 mr-2"
-        />
-        <p className="text-gray-200 text-l">avneeshagarwal0612@gmail.com</p>
-      </div>
-
-      <div className="flex ring-blue-700 hover:ring-2 p-2 rounded-lg cursor-pointer focus:outline-none w-10/12 ml-10 transform-gpu md:hidden">
-        <img
-          src="https://res.cloudinary.com/dssvrf9oz/image/upload/v1620391308/154-1541366_transparent-edit-icon-png-location-white-icon-png-removebg-preview_1_ewvzdm.png"
-          alt=""
-          className="h-7 ml-2 mr-2"
-        />
-        <p className="text-gray-200 text-l">India</p>
-      </div>
-      <div className="flex m-5 ml-10 md:hidden">
-        <a href="https:instagram.com/avneesh__agarwal" target="_blank">
-          <img
-            className="h-14 w-14 hover:bg-blue-900 rounded-full cursor-pointer transform-gpu "
-            src="https://res.cloudinary.com/dssvrf9oz/image/upload/v1620289544/md_5af2d4cabfdf2-removebg-preview_mwmvjh.png"
-            alt=""
-          />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/avneesh-agarwal-78312b20a/"
-          target="_blank"
-        >
-          <img
-            className="h-14 w-14 hover:bg-blue-900 rounded-full cursor-pointer transform-gpu"
-            src="https://res.cloudinary.com/dssvrf9oz/image/upload/v1620292929/1fdbd88fec469fc342cdff7ea25b8bd8-removebg-preview_vctiar.png"
-            alt=""
-          />
-        </a>
-      </div>
     </div>
   );
 }
