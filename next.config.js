c4dbc40d71f867a0aa2e687672f4b5aecc1777d0
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-05-08 12:12:08 +0530

    commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    userId: "user_7ZQgzGca2oteS5RRColwT",
+    templateId: "template_yjaky9d",
+    mailId: "gmail",
+    apiKey: "https://blog-site-backend-avneesh.herokuapp.com/blog/",
+  },
+};
